4ef6601878259dec50c157a28e0a28cbac6bf2f1
manthan-l-simformsolutions <manthan.l@simformsolutions.com>
2024-07-25 17:59:57 +0530

    Add home and client page content

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -9,8 +9,15 @@ const ClientPage = () => {
     return null;
   }
   return (
-    <div className="h-full flex flex-col items-center justify-center text-2xl">
-      Hello, {user.firstName} welcome to Clerk
+    <div className="h-full flex flex-col items-center justify-center space-y-4 text-2xl">
+      <div className="font-bold text-3xl">
+        Welcome to Clerk, {user.firstName}!
+      </div>
+      <p className="text-center text-lg max-w-lg">
+        We are excited to have you on board! Dive in and explore the wide range
+        of features we offer. Should you have any questions or need assistance,
+        our support team is here to ensure you have a seamless experience.
+      </p>
     </div>
   );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex items-center justify-center h-full">
-      <div className="text-2xl">Home Page</div>
+    <div className="flex flex-col items-center justify-center h-full space-y-8">
+      <div className="text-4xl font-bold">Welcome to Our Application!</div>
+      <p className="text-lg text-center max-w-2xl">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque,
+        magni saepe! Velit deserunt officiis necessitatibus iste sed quisquam,
+        ullam nulla alias distinctio facilis praesentium, suscipit, tenetur quos
+        ratione iure quasi? Architecto aut temporibus amet repudiandae, alias
+      </p>
     </div>
   );
 }
