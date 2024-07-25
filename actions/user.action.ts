6b90286881f4d45fccf420bc1cd308b12dde77a7
manthan-l-simformsolutions <manthan.l@simformsolutions.com>
2024-07-25 17:12:59 +0530

    clerk auth initial commit

---
+++ actions/user.action.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { connect } from "@/db";
+import User from "@/modals/user.modal";
+
+export async function createUser(user: any) {
+  try {
+    await connect();
+    const newUser = await User.create(user);
+    return JSON.parse(JSON.stringify(newUser));
+  } catch (error) {
+    console.log(error);
+  }
+}
